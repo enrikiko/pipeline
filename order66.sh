7eb3f0791cead8e32b5d939514496706d308fa89
enrikiko <enrikiko_91@hotmail.com>
2020-09-26 14:00:39 +0200

    Class across forward bring

---
+++ order66.sh
@@ -0,0 +1,11 @@
+docker-compose build \
+--build-arg PASSWORD=$GIT_PASSWORD \
+--build-arg ROUTE53_URL=$ROUTE53_URL \
+--build-arg ROUTE53_PASSWORD=$ROUTE53_PASSWORD \
+--build-arg ACCESS_KEY=$ACCESS_KEY \
+--build-arg SECRET_KEY=$SECRET_KEY \
+--build-arg BUCKETS_NAME=$BUCKETS_NAME \
+--build-arg API_URL=$API_URL \
+--build-arg API_KEY=$API_KEY
+sleep 5 
+docker-compose up -d
