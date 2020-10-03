35f66539124eeadb9c5f8df4c11e885959ad4c32
enrikiko <enrikiko_91@hotmail.com>
2020-10-03 21:59:31 +0200

    First yet someone once senior question

--- router53/init.sh
+++ router53/init.sh
@@ -3,11 +3,14 @@ while :
 do
   curl -o /dev/null -s -w "%{http_code}\n" app.cortijodemazas.com > /tmp/curl.out
   if [[ "$(cat /tmp/curl.out)" == "200" ]]; then
+    date
     echo "Route53 is set correctly"
   else
-    curl $ROUTE53_URL --header "Content-Type: application/json" --data '{"password":"'${ROUTE53_PASSWORD}'"}' 2>/dev/null > /tmp/curl.out
+    #curl $ROUTE53_URL --header "Content-Type: application/json" --data '{"password":"'${ROUTE53_PASSWORD}'"}' 2>/dev/null > /tmp/curl.out
+    curl -X POST -H "x-api-key: ${ROUTE53_PASSWORD}" $ROUTE53_URL 2>/dev/null > /tmp/curl.out
+    date
     cat /tmp/curl.out
-    echo "Route53 have been set correctly"
+    echo "Route53 have been set"
   fi
 	sleep 300
 done
